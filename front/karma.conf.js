e53930c4f62396da23a67cd19c292a84a8350417
Nobound <jean.ungerer.nobound@gmail.com>
2024-10-04 11:21:40 +0200

    Improve test coverage reporting
    Updated the npm test script to include code coverage generation and modified the Karma coverage output directory path. Additionally, adjusted the .gitignore to use a more general pattern for the site directory.

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bobapp'),
+      dir: require('path').join(__dirname, './coverage-report/bobapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
