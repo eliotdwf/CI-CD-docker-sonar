2002c284baf324b43b0220f3ea80f7fee896ed4a
eliotdwf <eliotdewulf91@gmail.com>
2024-11-06 00:11:19 +0100

    add sonar action config

--- back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
+++ back/src/main/java/com/openclassrooms/bobapp/controller/JokeController.java
@@ -1,11 +1,10 @@
 package com.openclassrooms.bobapp.controller;
 
+import com.openclassrooms.bobapp.service.JokeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.openclassrooms.bobapp.service.JokeService;
 
 @RestController
 @RequestMapping("api/joke")
